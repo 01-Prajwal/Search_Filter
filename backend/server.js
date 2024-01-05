8a7ecbc33d2d1145a7bd2ea205c5d10f8a674e59
Prajwal Bhange <95950835+01-Prajwal@users.noreply.github.com>
2024-01-05 10:25:35 +0530

    Add files via upload

---
+++ backend/server.js
@@ -0,0 +1,34 @@
+require("dotenv").config()
+const express = require('express')
+const cors = require("cors")
+const data = require('../backend/movies.json')
+const app = express()
+const MovieRouter = require("./Routes/Movie")
+const mongoose = require("mongoose")
+
+
+app.use(cors())
+app.use(express.json())
+
+const port = process.env.PORT||8080
+
+app.use('/api',MovieRouter)
+mongoose.connect(process.env.MONGO)
+
+
+.then(()=>{
+    console.log("DEVELOPMENT IN PROGRESS");
+    
+})
+.catch((err)=>{
+    console.log(err);
+    
+})
+
+
+app.listen(port,()=>{
+
+    console.log(`Listening on PORT${port}..`);
+    
+
+})
